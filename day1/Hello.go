395fa6cacdebc8e7940059b71802c79afc997170
KshirsagarR <manoramakshirsagar3@gmail.com>
2025-12-05 18:06:18 +0530

    changed

---
+++ day1/Hello.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+var name = "world"
+
+func hello() {
+
+	fmt.Printf("Hello, %v ", name)
+}
---
+++ day1/main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	hello()
+	// var name string
+	// var age int
+
+	// fmt.Println("Enter ur name")
+	// fmt.Scanln(&name)
+
+	// fmt.Print("enter ur age")
+	// fmt.Scanln(&age)
+
+	// fmt.Print("Hello", name, "ur age is", age)
+}
